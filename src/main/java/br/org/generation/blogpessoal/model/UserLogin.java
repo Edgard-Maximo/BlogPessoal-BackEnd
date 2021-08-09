5f5f6f8b20683200652c7f6797503a022d904685
Edgard Maximo <edgard.maximo@outlook.com>
2021-08-09 17:10:11 -0300

    Atualizando back-end

---
+++ src/main/java/br/org/generation/blogpessoal/model/UserLogin.java
@@ -0,0 +1,25 @@
+package br.org.generation.blogpessoal.model;
+
+import lombok.Data;
+
+@Data
+public class UserLogin {
+	
+	private long id; // id
+
+	private String nome; // ok
+
+	private String usuario;
+
+	private String senha; // ok
+
+	private String token; //ok
+	
+	private String foto; //ok
+	
+	private String tipo; // ok
+
+	
+
+
+}
